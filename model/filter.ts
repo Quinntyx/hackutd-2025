53caa290a92d8c581b798d170915ee1bcf05b823
quinntyx <harrisonhenry916@gmail.com>
2025-11-08 19:41:28 -0600

    feat: filter type

---
+++ model/filter.ts
@@ -0,0 +1,16 @@
+import { FuelType, Transmission } from "./data"
+
+export type CompoundFilter = {
+    priceTarget?: number
+    pricePriority: number
+    mpgTarget?: number
+    mpgPriority: number
+    transmission?: Transmission
+    transmissionPriority: number
+    electric?: boolean
+    electricPriority: number
+    mileageTarget?: number
+    mileagePriority: number
+    fuelType?: FuelType | undefined
+    fuelTypePriority: number
+}
